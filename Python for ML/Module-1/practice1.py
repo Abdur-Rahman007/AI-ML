ab00d867f0054098cd663c5a279eeffb670f1c37
Abdur Rahman <arrahman00700@gmail.com>
2025-11-02 22:32:54 +0600

    Starting from basic of python.

---
+++ Python for ML/Module-1/practice1.py
@@ -0,0 +1,12 @@
+
+x = 100
+y = 3.14
+z = "Abdur rahman"
+t = True
+
+print("Type           Value")
+print(f"{type(x)} -> {x}")
+print(f"{type(y)} -> {y}")
+print(f"{type(z)} -> {z}")
+print(f"{type(t)} -> {t}")
+
